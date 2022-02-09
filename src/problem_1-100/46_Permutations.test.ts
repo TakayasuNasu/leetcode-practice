b5e96f3429be6dd7dcff4f1507842fa325567acd
takayasu.nasu <taka.beckham@gmail.com>
2022-02-08 23:48:45 -0800

    Added problem 46

---
+++ src/problem_1-100/46_Permutations.test.ts
@@ -0,0 +1,29 @@
+import { permute } from './46_Permutations'
+
+test('0 test', () => {
+  const input = [1, 2, 3]
+  const output = [
+    [1, 2, 3],
+    [1, 3, 2],
+    [2, 1, 3],
+    [2, 3, 1],
+    [3, 1, 2],
+    [3, 2, 1],
+  ]
+  expect(permute(input)).toBe(output)
+})
+
+test('2 test', () => {
+  const input = [0, 1]
+  const output = [
+    [0, 1],
+    [1, 0],
+  ]
+  expect(permute(input)).toBe(output)
+})
+
+test('3 test', () => {
+  const input = [1]
+  const output = [[1]]
+  expect(permute(input)).toBe(output)
+})
---
+++ src/problem_1-100/46_Permutations.ts
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/permutations/
+ */
+
+/**
+ * Given an array nums of distinct integers,
+ * return all the possible permutations. You can return the answer in any order.
+ */
+export function permute(nums: number[]): number[][] {
+  return [[1]]
+}
+
+function helper(input: number[]) {
+  if (input.length == 0) {
+    return
+  }
+}
