dcef910ab09c3166a3ae869fffdb36817af81961
takayasu.nasu <taka.beckham@gmail.com>
2022-02-10 22:45:26 -0800

    Added Solution

--- src/problem_101-200/123_BestTimeToBuySellStockIII.test.ts
+++ src/problem_101-200/123_BestTimeToBuySellStockIII.test.ts
@@ -5,3 +5,15 @@ test('0 test', () => {
   const o = 6
   expect(maxProfit(prices)).toBe(o)
 })
+
+test('1 test', () => {
+  const prices = [1, 2, 3, 4, 5]
+  const o = 4
+  expect(maxProfit(prices)).toBe(o)
+})
+
+test('2 test', () => {
+  const prices = [7, 6, 4, 3, 1]
+  const o = 0
+  expect(maxProfit(prices)).toBe(o)
+})
