e25d466c05877b823809a5d9a6a34858779f6e01
takayasu.nasu <taka.beckham@gmail.com>
2022-02-10 22:44:28 -0800

    Added Solution

--- src/problem_101-200/123_BestTimeToBuySellStockIII.test.ts
+++ src/problem_101-200/123_BestTimeToBuySellStockIII.test.ts
@@ -1,3 +1,7 @@
 import { maxProfit } from './123_BestTimeToBuySellStockIII'
 
-test('0 test', () => {})
+test('0 test', () => {
+  const prices = [3, 3, 5, 0, 0, 3, 1, 4]
+  const o = 6
+  expect(maxProfit(prices)).toBe(o)
+})
--- src/problem_101-200/123_BestTimeToBuySellStockIII.ts
+++ src/problem_101-200/123_BestTimeToBuySellStockIII.ts
@@ -10,5 +10,32 @@
  */
 
 export function maxProfit(prices: number[]): number {
-  return 1
+  if (prices.length < 2) return 0
+  let leftMin = Number.MAX_SAFE_INTEGER
+  let rightMax = Number.MIN_SAFE_INTEGER
+
+  let leftProfit = 0
+  let rightProfit = 0
+
+  const leftArray: number[] = new Array(prices.length)
+  const rightArray: number[] = new Array(prices.length)
+
+  for (let i = 0; i < prices.length; i++) {
+    leftMin = Math.min(leftMin, prices[i])
+    leftProfit = Math.max(leftProfit, prices[i] - leftMin)
+    leftArray[i] = leftProfit
+
+    rightMax = Math.max(rightMax, prices[prices.length - 1 - i])
+    rightProfit = Math.max(
+      rightProfit,
+      rightMax - prices[prices.length - 1 - i]
+    )
+    rightArray[prices.length - 1 - i] = rightProfit
+  }
+
+  const mergedArray = leftArray.map((leftValue, i) => leftValue + rightArray[i])
+  console.log(leftArray)
+  console.log(rightArray)
+
+  return Math.max(...mergedArray)
 }
