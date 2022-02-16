a1c32b0a43ea4801fd80872459053b27a4e43a8e
takayasu.nasu <taka.beckham@gmail.com>
2022-02-16 00:55:26 -0800

    Added Test case

--- src/problem_101-200/199_BinaryTreeRightSideView.test.ts
+++ src/problem_101-200/199_BinaryTreeRightSideView.test.ts
@@ -5,3 +5,13 @@ test('0 test', () => {
   const root = fromArrayToTree([1, 2, 3, null, 5, null, 4])
   expect(rightSideView(root)).toStrictEqual([1, 3, 4])
 })
+
+test('1 test', () => {
+  const root = fromArrayToTree([1, null, 3])
+  expect(rightSideView(root)).toStrictEqual([1, 3])
+})
+
+test('2 test', () => {
+  const root = fromArrayToTree([])
+  expect(rightSideView(root)).toStrictEqual([])
+})
