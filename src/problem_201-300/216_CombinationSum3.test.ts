05154679aea9e21bac421fcf247ba89e05847b3d
takayasu.nasu <taka.beckham@gmail.com>
2022-02-17 23:42:30 -0800

    Added Onther Test case

--- src/problem_201-300/216_CombinationSum3.test.ts
+++ src/problem_201-300/216_CombinationSum3.test.ts
@@ -6,3 +6,14 @@ test('0 test', () => {
   const o = [[1, 2, 4]]
   expect(combinationSum3(k, n)).toStrictEqual(o)
 })
+
+test('1 test', () => {
+  const k = 3
+  const n = 9
+  const o = [
+    [1, 2, 6],
+    [1, 3, 5],
+    [2, 3, 4],
+  ]
+  expect(combinationSum3(k, n)).toStrictEqual(o)
+})
