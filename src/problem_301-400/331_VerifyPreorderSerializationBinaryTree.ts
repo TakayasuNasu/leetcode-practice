1410d01232d8fc73c4fcee38058d7c008e6e9881
takayasu.nasu <taka.beckham@gmail.com>
2022-02-23 00:07:39 -0800

    Added Base Code

---
+++ src/problem_301-400/331_VerifyPreorderSerializationBinaryTree.ts
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ *
+ * One way to serialize a binary tree is to use preorder traversal.
+ * When we encounter a non-null node, we record the node's value.
+ * If it is a null node, we record using a sentinel value such as '#'.
+ */
+
+export function isValidSerialization(preorder: string): boolean {
+  return false
+}
