adf6c7d746fc494dec09143b2f0dd7e2c562a42f
takayasu.nasu <taka.beckham@gmail.com>
2022-03-06 23:54:19 -0800

    Modified code

---
+++ src/problem_401-500/451_SortCharactersFrequency.ts
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ *
+ * Given a string s, sort it in decreasing order based on the frequency of the characters.
+ * The frequency of a character is the number of times it appears in the string.
+ *
+ * Return the sorted string. If there are multiple answers, return any of them.
+ */
+
+export function frequencySort(s: string): string {
+  return ''
+}
