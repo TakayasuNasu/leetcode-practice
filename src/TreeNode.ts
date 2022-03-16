7b50b045ed3c2878a9a1234726b9869212b71dc9
takayasu.nasu <taka.beckham@gmail.com>
2022-03-16 01:17:41 -0700

    Modified array to tree converter

--- src/TreeNode.ts
+++ src/TreeNode.ts
@@ -14,14 +14,16 @@ export const fromArrayToTree = (
   root: Array<number | null>
 ): TreeNode | null => {
   if (root.length == 0) return null
-  if (!root[0]) return null
-  const v = root.shift() ?? undefined
-  const l = root.shift() ?? null
-  const r = root.shift() ?? null
-  const i = Math.floor(root.length / 2)
-  return new TreeNode(
-    v,
-    fromArrayToTree([l, ...root.slice(0, i)]),
-    fromArrayToTree([r, ...root.slice(i)])
-  )
+  if (root == null || root.length == 0) {
+    return null
+  }
+  return toTree(root, 0)
+}
+
+const toTree = (root: Array<number | null>, i: number): TreeNode | null => {
+  if (i >= root.length) return null
+  if (!root[i]) return null
+  const v = root[i]!
+
+  return new TreeNode(v, toTree(root, i * 2 + 1), toTree(root, i * 2 + 2))
 }
