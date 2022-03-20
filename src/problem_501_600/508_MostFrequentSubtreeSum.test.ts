da61a7034b302b97b7a5113dbbc6586cc6c4f1b5
takayasu.nasu <taka.beckham@gmail.com>
2022-03-19 23:20:24 -0700

    Added Test Case

---
+++ src/problem_501_600/508_MostFrequentSubtreeSum.test.ts
@@ -0,0 +1,8 @@
+import { fromArrayToTree } from '@/TreeNode'
+import { findFrequentTreeSum } from './508_MostFrequentSubtreeSum'
+
+test('0 test', () => {
+  const root = fromArrayToTree([5, 2, -3])
+  const o = [2, -3, 4]
+  expect(findFrequentTreeSum(root)).toBe(o)
+})
