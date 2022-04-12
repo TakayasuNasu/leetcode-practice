200afd1d12900f657b596dac193d25609c036eb7
takayasu.nasu <taka.beckham@gmail.com>
2022-04-11 23:37:22 -0700

    Add New Problem

---
+++ src/problem_801-900/897_IncreasingOrderSearchTree.ts
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/increasing-order-search-tree/
+ *
+ * Given the root of a binary search tree,
+ * rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree,
+ * and every node has no left child and only one right child.
+ */
+
+import { TreeNode } from '@/TreeNode'
+
+export function increasingBST(root: TreeNode | null): TreeNode | null {
+  return null
+}
