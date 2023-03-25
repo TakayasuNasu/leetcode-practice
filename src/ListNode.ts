0d88e2b6eb7723b7e2756aa0bae67d5adadaa10a
takayasu.nasu <taka.beckham@gmail.com>
2023-03-24 23:14:50 -0700

    Use Vite and Vitest

---
+++ src/ListNode.ts
@@ -0,0 +1,25 @@
+export class ListNode {
+  val: number
+  next: ListNode | null
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export const toListNode = (array: number[]): ListNode | null => {
+  const head: ListNode | null = array.reduce(
+    (acc: ListNode | null, current: number) => {
+      if (acc == null) {
+        acc = new ListNode(current)
+      } else {
+        acc = new ListNode(current, acc)
+      }
+      return acc
+    },
+    null
+  )
+
+  return head
+}
